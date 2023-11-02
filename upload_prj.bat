c7ee9f32685f0abe5b165bb91498c8f42fcaa2a7
M L <martinn.lindbladd@gmail.com>
2023-11-02 12:22:00 +0100

    admin css base

--- upload_prj.bat
+++ upload_prj.bat
@@ -1,4 +1,4 @@
 cd \Users\marti\Desktop\Django\trees_db_Project
 scp -i ~/.ssh/id_rsa.gamechanger.ml -r treesdb/* deploy@www.gamechanger.eco:/home/deploy/treesdb
-#scp -i ~/.ssh/id_rsa.gamechanger.ml -r t_env/* deploy@www.gamechanger.eco:/home/deploy/.local/share/virtualenvs/t_env/
+
 pause 
